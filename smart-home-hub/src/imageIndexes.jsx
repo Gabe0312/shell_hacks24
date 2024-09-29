e13a5bb976f5f95d147e313e90293478162805db
Eilyn Tudares <etuda001@fiu.edu>
2024-09-28 21:25:56 -0400

    imageloader and imageindexes

---
+++ smart-home-hub/src/imageIndexes.jsx
@@ -0,0 +1,22 @@
+import { images } from "./imageLoader"; 
+
+export const imageAt = {
+  coffeeMachine: images[0],
+  alexa: images[1],
+  healthApp: images[2],
+  bathroom: images[3],
+  bedroom: images[4],
+  canva: images[5],
+  fridge: images[6],
+  humidity: images[7],
+  kitchen: images[8],
+  light: images[9],
+  linkedIn: images[10],
+  livingRoom: images[11],
+  smartphone: images[12],
+  tv: images[13],
+  outlook: images[14],
+  temperature: images[15],
+  wifi: images[16],
+  logo:[17],
+};
---
+++ smart-home-hub/src/imageLoader.jsx
@@ -0,0 +1,42 @@
+import coffeeIcon from "./assets/coffee-machine-4.png";
+import alexaIcon from "./assets/Alexa_Logo_RGB_SQUID.png";
+import healthAppIcon from "./assets/Apple_Health-512.webp";
+import bathroomIcon from "./assets/bathroom.png";
+import bedroomIcon from "./assets/bedroom.png";
+import canvaIcon from "./assets/canva.png";
+import coffeeMachineIcon from "./assets/coffee-machine-4.png";
+import fridgeIcon from "./assets/fridge.png";
+import humidityIcon from "./assets/humidity.png";
+import kitchenIcon from "./assets/kitchen.png";
+import lightIcon from "./assets/light.png";
+import linkedInIcon from "./assets/inkedin-logo-linkedin-icon-transparent-free-png.webp";
+import livingRoomIcon from "./assets/linvingroom.png";
+import smartphoneIcon from "./assets/smartphone.png";
+import tvIcon from "./assets/smarttv.png";
+import outlook from "./assets/tech-guide_header-image-outlook.webp";
+import temperatureIcon from "./assets/temperature.png";
+import wifiIcon from "./assets/wifi.png";
+import logo from "./SmartHome-2.png";
+
+// Exporting all images in an array or object
+export const images = [
+coffeeIcon, 
+alexaIcon,
+healthAppIcon, 
+bathroomIcon, 
+bedroomIcon, 
+canvaIcon, 
+coffeeMachineIcon, 
+fridgeIcon, 
+humidityIcon, 
+kitchenIcon, 
+lightIcon, 
+linkedInIcon, 
+livingRoomIcon, 
+smartphoneIcon, 
+tvIcon, 
+outlook, 
+temperatureIcon, 
+wifiIcon, 
+logo
+];
